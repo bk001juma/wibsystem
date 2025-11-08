70177eb742779ab632208d934865ffea385556b0
bk001juma <juannabarack001@gmail.com>
2025-11-09 00:41:39 +0300

    Fix: image path

--- src/lib/utils/imagePath.ts
+++ src/lib/utils/imagePath.ts
@@ -1,5 +1,1 @@
-export const getImagePath = (path: string): string => {
-  const basePath =
-    process.env.NODE_ENV === "production" ? "/crypto-nextjs" : "";
-  return `${basePath}${path}`;
-};
+export const getImagePath = (path: string): string => path;
