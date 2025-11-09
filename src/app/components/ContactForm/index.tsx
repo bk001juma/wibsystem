53337d41c5eb1ced68e796f0853b8a0882273830
bk001juma <juannabarack001@gmail.com>
2025-11-09 03:56:38 +0300

    form fix

--- src/app/components/ContactForm/index.tsx
+++ src/app/components/ContactForm/index.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -10,9 +9,7 @@ const ContactForm = () => {
     phnumber: "",
     Message: "",
   });
-  const [submitted, setSubmitted] = useState(false);
-  const [showThanks, setShowThanks] = useState(false);
-  const [loader, setLoader] = useState(false);
+
   const [isFormValid, setIsFormValid] = useState(false);
 
   useEffect(() => {
@@ -21,6 +18,7 @@ const ContactForm = () => {
     );
     setIsFormValid(isValid);
   }, [formData]);
+
   const handleChange = (e: any) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
@@ -28,55 +26,16 @@ const ContactForm = () => {
       [name]: value,
     }));
   };
-  const reset = () => {
-    formData.firstname = "";
-    formData.lastname = "";
-    formData.email = "";
-    formData.phnumber = "";
-    formData.Message = "";
-  };
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    setLoader(true);
 
-    fetch("https://formsubmit.co/el/dowina", {
-      method: "POST",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({
-        Name: formData.firstname,
-        LastName: formData.lastname,
-        Email: formData.email,
-        PhoneNo: formData.phnumber,
-        Message: formData.Message,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.success) {
-          setSubmitted(true);
-          setShowThanks(true);
-          reset();
-
-          setTimeout(() => {
-            setShowThanks(false);
-          }, 5000);
-        }
-
-        reset();
-      })
-      .catch((error) => {
-        setLoader(false);
-        console.log(error.message);
-      });
-  };
   return (
     <section id="contact" className="scroll-mt-14">
       <div className="container">
         <div className="relative">
-          <h2 className="mb-9  capitalize">Get in Touch</h2>
+          <h2 className="mb-9 capitalize">Get in Touch</h2>
           <div className="relative border border-lightblue/35 px-6 py-2 rounded-2xl">
             <form
-              onSubmit={handleSubmit}
+              action="https://formsubmit.co/el/dowina"
+              method="POST"
               className="flex flex-wrap w-full m-auto justify-between"
             >
               <div className="sm:flex gap-6 w-full">
@@ -90,10 +49,11 @@ const ContactForm = () => {
                   <input
                     id="fname"
                     type="text"
-                    name="firstname"
+                   name="firstname"
                     value={formData.firstname}
                     onChange={handleChange}
                     placeholder="John"
+                    required
                     className="w-full text-base px-4 rounded-2xl py-2.5 border-lightblue/35 border transition-all duration-500 focus:border-primary focus:outline-0 placeholder:text-lightsky/40 text-white"
                   />
                 </div>
@@ -111,10 +71,12 @@ const ContactForm = () => {
                     value={formData.lastname}
                     onChange={handleChange}
                     placeholder="Doe"
+                    required
                     className="w-full text-base px-4 rounded-2xl py-2.5 border-lightblue/35 border transition-all duration-500 focus:border-primary focus:outline-0 placeholder:text-lightsky/40 text-white"
                   />
                 </div>
               </div>
+
               <div className="sm:flex gap-6 w-full">
                 <div className="mx-0 my-2.5 flex-1">
                   <label
@@ -130,6 +92,7 @@ const ContactForm = () => {
                     value={formData.email}
                     onChange={handleChange}
                     placeholder="john.doe@example.com"
+                    required
                     className="w-full text-base px-4 rounded-2xl py-2.5 border-lightblue/35 border transition-all duration-500 focus:border-primary focus:outline-0 placeholder:text-lightsky/40 text-white"
                   />
                 </div>
@@ -144,13 +107,15 @@ const ContactForm = () => {
                     id="Phnumber"
                     type="tel"
                     name="phnumber"
-                    placeholder="+1234567890"
                     value={formData.phnumber}
                     onChange={handleChange}
+                    placeholder="+1234567890"
+                    required
                     className="w-full text-base px-4 rounded-2xl py-2.5 border-lightblue/35 border transition-all duration-500 focus:border-primary focus:outline-0 placeholder:text-lightsky/40 text-white"
                   />
                 </div>
               </div>
+
               <div className="w-full mx-0 my-2.5 flex-1">
                 <label
                   htmlFor="message"
@@ -163,17 +128,19 @@ const ContactForm = () => {
                   name="Message"
                   value={formData.Message}
                   onChange={handleChange}
-                  className="w-full mt-2 rounded-2xl px-5 py-3 border-lightblue/35 border transition-all duration-500 focus:border-primary focus:outline-0 placeholder:text-lightsky/40 text-white"
                   placeholder="Anything else you wanna communicate"
+                  required
+                  className="w-full mt-2 rounded-2xl px-5 py-3 border-lightblue/35 border transition-all duration-500 focus:border-primary focus:outline-0 placeholder:text-lightsky/40 text-white"
                 ></textarea>
               </div>
+
               <div className="mx-0 my-2.5 w-full">
                 <button
                   type="submit"
-                  disabled={!isFormValid || loader}
+                  disabled={!isFormValid}
                   className={`border leading-none px-6 text-lg font-medium py-4 rounded-full 
                     ${
-                      !isFormValid || loader
+                      !isFormValid
                         ? "bg-gray-300 text-gray-500 cursor-not-allowed"
                         : "bg-primary border-primary text-white hover:bg-transparent hover:text-primary cursor-pointer"
                     }`}
@@ -183,12 +150,6 @@ const ContactForm = () => {
               </div>
             </form>
           </div>
-          {showThanks && (
-            <div className="text-white bg-primary rounded-full px-4 text-lg mb-4.5 mt-1 absolute flex items-center gap-2">
-              Thank you for contacting us! We will get back to you soon.
-              <div className="w-3 h-3 rounded-full animate-spin border-2 border-lightblue border-t-transparent"></div>
-            </div>
-          )}
         </div>
       </div>
     </section>
